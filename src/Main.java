303d5c9c098e46b26f15b4aa3e3c729be73f0a13
Yousuke Nakamura <e165717@ie.u-ryukyu.ac.jp>
2016-12-13 02:56:40 +0900

    ステップ１

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by e165717 on 2016/12/13.
+ */
+public class Main {
+    public static void main(String arg[]){
+        String str = null;
+
+        System.out.println(str.length());
+    }
+}
+
