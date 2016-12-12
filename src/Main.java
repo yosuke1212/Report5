22109d0a2532883cff1416f26430a7be3f1d0833
Yousuke Nakamura <e165717@ie.u-ryukyu.ac.jp>
2016-12-13 03:32:45 +0900

    ステップ2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,21 @@
  * Created by e165717 on 2016/12/13.
  */
 public class Main {
-    public static void main(String arg[]){
-        String str = null;
+    public static void main(String arg[]) {
+        try {
+            String str;
+            int len;
+            str = "3. 14";
+            double value = Double.parseDouble(str);
+            len = str.length();
+            System.out.println(len);
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました。");
+            e.printStackTrace();
+            return;
+        }
 
-        System.out.println(str.length());
     }
 }
+
 
